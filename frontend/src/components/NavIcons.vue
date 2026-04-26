a7ecda5315ade9b0983518cd75bdbfa28f40fbe4
Ivan R. Judson <irjudson@gmail.com>
2026-04-26 09:47:57 -0600

    feat: add SVG nav icon component replacing emoji icons

---
+++ frontend/src/components/NavIcons.vue
@@ -0,0 +1,61 @@
+<template>
+  <component :is="'svg'" xmlns="http://www.w3.org/2000/svg" :width="size" :height="size"
+    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+    stroke-linecap="round" stroke-linejoin="round" :class="className">
+    <template v-if="name === 'home'">
+      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+      <polyline points="9 22 9 12 15 12 15 22"/>
+    </template>
+    <template v-else-if="name === 'graphs'">
+      <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"/>
+    </template>
+    <template v-else-if="name === 'import'">
+      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+      <polyline points="7 10 12 15 17 10"/>
+      <line x1="12" y1="15" x2="12" y2="3"/>
+    </template>
+    <template v-else-if="name === 'analysis'">
+      <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/>
+    </template>
+    <template v-else-if="name === 'explorer'">
+      <circle cx="11" cy="11" r="8"/>
+      <line x1="21" y1="21" x2="16.65" y2="16.65"/>
+    </template>
+    <template v-else-if="name === 'settings'">
+      <circle cx="12" cy="12" r="3"/>
+      <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83-2.83l.06-.06A1.65 1.65 0 0 0 4.68 15a1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 2.83-2.83l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z"/>
+    </template>
+    <template v-else-if="name === 'menu'">
+      <line x1="3" y1="6" x2="21" y2="6"/>
+      <line x1="3" y1="12" x2="21" y2="12"/>
+      <line x1="3" y1="18" x2="21" y2="18"/>
+    </template>
+    <template v-else-if="name === 'chevron-left'">
+      <polyline points="15 18 9 12 15 6"/>
+    </template>
+    <template v-else-if="name === 'chevron-right'">
+      <polyline points="9 18 15 12 9 6"/>
+    </template>
+    <template v-else-if="name === 'x'">
+      <line x1="18" y1="6" x2="6" y2="18"/>
+      <line x1="6" y1="6" x2="18" y2="18"/>
+    </template>
+    <template v-else-if="name === 'wifi-off'">
+      <line x1="1" y1="1" x2="23" y2="23"/>
+      <path d="M16.72 11.06A10.94 10.94 0 0 1 19 12.55"/>
+      <path d="M5 12.55a10.94 10.94 0 0 1 5.17-2.39"/>
+      <path d="M10.71 5.05A16 16 0 0 1 22.56 9"/>
+      <path d="M1.42 9a15.91 15.91 0 0 1 4.7-2.88"/>
+      <path d="M8.53 16.11a6 6 0 0 1 6.95 0"/>
+      <line x1="12" y1="20" x2="12.01" y2="20"/>
+    </template>
+  </component>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  size?: number;
+  className?: string;
+}>();
+</script>
